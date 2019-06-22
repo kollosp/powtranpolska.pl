0d1785866e59d38f0cd56b94f642627b02767cf2
kollosp <pparczyk@wp.pl>
2019-06-22 22:00:17 +0200

    basic navigation aand beggining of contact, products, download, catalog and prices pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { render: req.render });
+	res.render('index', { render: req.render });
 });
+
+/* GET download. */
+router.get('/download', function(req, res, next) {
+
+	req.render.download = [{date: "25.04.19", name: "Instrukcja obsługi PI8000", path: "/jakas/sciezka/do/pliku"},
+		{date: "25.04.19", name: "Instrukcja obsługo PI8000", path: "/jakas/sciezka/do/pliku"},
+		{date: "25.04.19", name: "Instrukcja obsługo PI8000", path: "/jakas/sciezka/do/pliku"}]
+
+	res.render('download', { render: req.render });
+});
+
+/* GET prices. */
+router.get('/prices', function(req, res, next) {
+	res.render('prices', { render: req.render });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+	res.render('contact', { render: req.render });
+});
+
+/* GET products (menu) page. */
+router.get('/products', function(req, res, next) {
+	res.render('products', { render: req.render });
+});
+
 
 module.exports = router;
--- routes/middleware.js
+++ routes/middleware.js
@@ -5,8 +5,9 @@ var router = express.Router();
 router.use('/', function(req, res, next) {
 
 	req.render = {
-		news: [{title: "Seria PI8XXXX", date: "31.02.17", mainImage: "images/inverter1.png", brief: "W naszej ofercie pojawily się kolejne moce falowników trójfazowych serii 8XXX zapoznaj się z ofertą"},
-			   {title: "Seria PI9XXXX", date: "25.08.18", mainImage: "images/inverter2.png", brief: "Najnowsza seria falowników PI9XXXX"}]
+		news: [{title: "Seria PI8XXXX", date: "31.02.17", mainImage: "/images/inverter1.png", brief: "W naszej ofercie pojawily się kolejne moce falowników trójfazowych serii 8XXX zapoznaj się z ofertą"},
+			   {title: "Seria PI9XXXX", date: "25.08.18", mainImage: "/images/inverter2.png", brief: "Najnowsza seria falowników PI9XXXX"}],
+	    mainBanner: "/images/banner.png" 
 	}
 
 	next()
